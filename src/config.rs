b4c5c2939464ba3044e2b73ab00cbfb1b712cb09
Dave Korhumel <dpk2442@gmail.com>
2021-08-17 23:51:51 -0700

    add default values for host and port

--- src/config.rs
+++ src/config.rs
@@ -2,15 +2,34 @@ use serde::Deserialize;
 use std::error;
 use toml;
 
+macro_rules! config_defaults {
+    ($($name:ident -> $type:ty: $value:expr;)*) => {
+    $(
+        fn $name() -> $type {
+            $value
+        }
+    )*
+    }
+}
+
+config_defaults! {
+    default_host -> String: "0.0.0.0".to_string();
+    default_port -> i32: 25565;
+}
+
 #[derive(Deserialize, Debug)]
 pub struct Config {
     name: String,
-    host: Option<String>,
-    port: Option<i32>,
+
+    #[serde(default = "default_host")]
+    host: String,
+
+    #[serde(default = "default_port")]
+    port: i32,
 }
 
 pub fn load_config(file_path: &str) -> Result<Config, Box<dyn error::Error>> {
-    debug!("Loading config from {}", file_path);
+    log::debug!("Loading config from {}", file_path);
     let file_contents = std::fs::read_to_string(file_path)?;
     Ok(toml::from_str(&file_contents)?)
 }
